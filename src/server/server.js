6a69f8b956be0c8416cda63ad285b665fcac5f27
nglitssoHard <dzikraiffat@gmail.com>
2024-06-03 16:21:25 +0700

    test2

--- src/server/server.js
+++ src/server/server.js
@@ -1,21 +1,29 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
-const routes = require('../server/routes');
+const routes = require('./routes');
 
 const init = async () => {
   const server = Hapi.server({
-    port: process.env.PORT,
-    host: '0.0.0.0',
-    routes: {
-      cors: {
-        origin: ['*'],
-      },
-  },
+      port: process.env.PORT || 8080,
+      host: '0.0.0.0',
+      routes: {
+          cors: {
+              origin: ['*'],
+              headers: ['Authorization', 'Content-Type', 'If-None-Match'],
+              additionalHeaders: ['cache-control', 'x-requested-with']
+          }
+      }
   });
 
   server.route(routes);
 
   await server.start();
-  console.log(`Server berjalan pada ${server.info.uri}`);
+  console.log('Server running on %s', server.info.uri);
 };
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
 
 init();
--- src/service/firestore.js
+++ src/service/firestore.js
@@ -1,4 +1,6 @@
 const { Firestore } = require('@google-cloud/firestore');
+require('dotenv').config();
+
 const firestore = new Firestore({
     projectId: 'dermascan-424815',
     keyFilename: process.env.GOOGLE_APPLICATION_CREDENTIALS
